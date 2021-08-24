ca538ba2716c32f71cc5ac92082ac118b62a53c5
Simone Benedetti <simone.benedetti@alumni.uniroma2.eu>
2021-08-24 11:03:46 +0200

    updating final tests

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
@@ -1,8 +1,11 @@
 package org.apache.bookkeeper.bookie;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -20,7 +23,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
-public class TestFileInfo {
+public class FileInfoTest {
 	
 	private static File lf;
 	private byte[] masterKey; //valid, not valid,"",null
@@ -34,8 +37,15 @@ public class TestFileInfo {
 	private long sizeContent;//<0,=0,>0
 	private static String fileName = "writeFileInfo.log";
 	private static int lengthByteBuff;
+	private ByteBuf lac;
+	private static ByteBuffer b1;
 
-	public TestFileInfo(TestInput ti) {
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
+
+
+
+	public FileInfoTest(TestInput ti) {
 		this.masterKey = ti.getMasterKey();
 		this.fileInfoVersion = ti.getFileInfoVersion();
 		this.dataTest1= ti.getDataTest1();
@@ -45,15 +55,32 @@ public class TestFileInfo {
 		this.bestEffort = ti.getBestEffort();
 		this.dst = ti.getDst();
 		this.sizeContent = ti.getSizeContent();
+		this.lac = ti.getLac();
 	}
 
 	@BeforeClass
-	public static void setUp() throws IOException {
+	public static void setUpEnvironment() {
 		// Create the directories if do not exist
 		if (!Files.exists(Paths.get("tmp"))) {
 			File tmpDir = new File("tmp");
 			tmpDir.mkdir();
 		}
+
+		// Delete test file if exists
+		if (Files.exists(Paths.get("tmp", fileName))) {
+			File testFile = new File("tmp", fileName);
+			testFile.delete();
+		}
+
+		if (Files.exists(Paths.get("tmp","temp.log"))) {
+			File testFile = new File("tmp", "temp.log");
+			testFile.delete();
+		}
+
+	}
+
+	@Before
+	public void setup() throws IOException {
 
 		lf = new File("tmp"+"/"+ fileName);
 		lf.createNewFile();
@@ -61,15 +88,12 @@ public class TestFileInfo {
 	}
 
 	@Parameterized.Parameters
-	public static Collection<TestInput[]> getTestParameters() throws IOException {
+	public static Collection<TestInput> getTestParameters() throws IOException {
 		Collection<TestInput> inputs = new ArrayList<>();
 
-		Collection<TestInput[]> result = new ArrayList<>();
 
-
-		//FileInfo fi = new FileInfo(lf, this.masterKey,1);
-		byte[] test = "Hello Worldhsjoeondnskkekooekndnbhieijdnsnjooaebrcjsosoeknd".getBytes(StandardCharsets.UTF_8);
-		ByteBuffer b1 = ByteBuffer.wrap(test);
+		byte[] test = "Test String".getBytes(StandardCharsets.UTF_8);
+		b1 = ByteBuffer.wrap(test);
 		ByteBuffer[] bb = new ByteBuffer[5];
 		bb[0] = b1;
 		bb[1] = b1;
@@ -79,28 +103,45 @@ public class TestFileInfo {
 
 		File temp = new File("tmp"+"/"+ "temp.log");
 		temp.createNewFile();
-		inputs.add(new TestInput("master".getBytes(StandardCharsets.UTF_8),FileInfo.CURRENT_HEADER_VERSION ,bb,bb,1L,20,false,temp,9223372036854775807L));
-		inputs.add(new TestInput("".getBytes(StandardCharsets.UTF_8),FileInfo.CURRENT_HEADER_VERSION,bb,bb,1L,20,false,temp,9223372036854775807L));
+		ByteBuf byB = Unpooled.buffer(0);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+		byB.writeLong(0L);
+
+		inputs.add(new TestInput(byB,"master".getBytes(StandardCharsets.UTF_8),FileInfo.V1 ,bb,bb,0L,55,true,temp,9223372036854775807L));
+		inputs.add(new TestInput(Unpooled.buffer(0),"".getBytes(StandardCharsets.UTF_8),FileInfo.V1,new ByteBuffer[]{ByteBuffer.allocate(2)},bb,0L,2,false,temp,9223372036854775807L));
+		inputs.add(new TestInput(byB,"master".getBytes(StandardCharsets.UTF_8),FileInfo.V1 ,bb,bb,0L,400,false,temp,0));
+		inputs.add(new TestInput(byB,"master".getBytes(StandardCharsets.UTF_8),FileInfo.V1 ,bb,bb,0L,400,true,temp,0));
+		inputs.add(new TestInput(byB,"master".getBytes(StandardCharsets.UTF_8),FileInfo.V0 ,bb,bb,0L,55,false,new File("tmp"+"/"+ fileName),9223372036854775807L));
+
+		//inputs.add(new TestInput(byB,"master".getBytes(StandardCharsets.UTF_8),FileInfo.V0 ,,bb,0L,55,false,new File("tmp"+"/"+ fileName),9223372036854775807L));
+
 		//inputs.add(new TestInput(null,FileInfo.CURRENT_HEADER_VERSION,bb,bb,1L,20,false,temp,Long.MAX_VALUE));
 		//inputs.add(new TestInput("master".getBytes(StandardCharsets.UTF_8),3,bb,bb,0L,20,true,temp,Long.MAX_VALUE));
 		//inputs.add(new TestInput(null,FileInfo.CURRENT_HEADER_VERSION,bb,bb,1L,20,false,temp,Long.MAX_VALUE));
 
-		for (TestInput e : inputs) {
-			result.add(new TestInput[] { e });
-		}
-		return result;
+		return inputs;
 
 	}
 
-	@AfterClass
-	public static void tearDown() {
+	@After
+	public void tearDown() throws IOException {
 		
 		  // Delete test file if exists
 		 if (Files.exists(Paths.get("tmp"))) {
-		     File testFile = new File("tmp",fileName);
-		     testFile.delete();
-			 File testFile2 = new File("tmp","temp.log");
-			 testFile2.delete();
+			 File testFile = new File("tmp"+"/"+"temp.log");
+			 if(Files.exists(Paths.get("tmp"+"/"+"temp.log")) && !testFile.delete()){
+				 throw new IOException("tear down file cannot deleted");
+			 }
+			 File testFile2 = new File("tmp"+"/"+fileName);
+			 if(Files.exists(Paths.get("tmp"+"/"+fileName)) && !testFile2.delete()){
+				 throw new IOException("tear down file2 cannot deleted");
+			 }
 		 }
 	}
 
@@ -114,8 +155,10 @@ public class TestFileInfo {
 		private Boolean bestEffort;
 		private File dst;
 		private long sizeContent;
+		private ByteBuf lac;
 
-		public TestInput(byte[] masterKey, int fileInfoVersion, ByteBuffer[] dataTest1,ByteBuffer[] dataTest2, long position, Integer size, Boolean bestEffort, File dst, long sizeContent) {
+		public TestInput(ByteBuf lac,byte[] masterKey, int fileInfoVersion, ByteBuffer[] dataTest1,ByteBuffer[] dataTest2, long position, Integer size, Boolean bestEffort, File dst, long sizeContent) {
+			this.lac = lac;
 			this.masterKey = masterKey;
 			this.fileInfoVersion = fileInfoVersion;
 			this.dataTest1 = dataTest1;
@@ -126,6 +169,12 @@ public class TestFileInfo {
 			this.dst = dst;
 			this.sizeContent = sizeContent;
 		}
+
+
+		public ByteBuf getLac() {
+			return lac;
+		}
+
 
 		public byte[] getMasterKey() {
 			return masterKey;
@@ -175,44 +224,47 @@ public class TestFileInfo {
 		assertEquals(masterK, fiMasterK);
 		assertEquals(lf,fi.getLf());
 
+		if(!fi.isDeleted()){
+			Logger.getRootLogger().setLevel(Level.DEBUG);
+			if(this.lac.capacity() != 0) {
+				fi.setExplicitLac(this.lac);
+				assertEquals(this.lac.resetReaderIndex(), fi.getExplicitLac());
+			}
+
+		}
+
 		fi.close(true);
 	}
 	
 	@Test
 	public void testWriteAndRead() throws IOException {
-		long bytes = 0;
+
 		FileInfo fi = new FileInfo(lf,this.masterKey,this.fileInfoVersion);
 		
 		long numBytesWritten = fi.write(this.dataTest1,this.position);
+		b1.position(0);
 
-		if (lf.exists()) {
-			// size of a file (in bytes)
-			bytes = lf.length();
-		}
 
-		if(numBytesWritten != 0L  && (bytes - 1025) > 0) {
-			assertEquals(bytes - 1025L, numBytesWritten);
-		}
+		assertTrue(numBytesWritten > 0L);
+		assertTrue(fi.size() > 0 );
+		assertEquals(fi.size() , numBytesWritten); //bytes - 1025L
 
 
 		ByteBuffer bBuff = ByteBuffer.allocate(this.size);
 
-		if(numBytesWritten !=0L) {
-			fi.read(bBuff, this.position, this.bestEffort);
-
-			ByteBuffer mergedBb = ByteBuffer.allocate(this.size);
-
-			for (int i = 0; i < this.dataTest1.length; i++) {
-
-				while (this.dataTest1[i].hasRemaining())
-					mergedBb.put(this.dataTest1[i].get());
-
-			}
-			assertEquals(20, mergedBb.compareTo(bBuff));
-
-			fi.close(true);
+		if(this.size > 55 && this.bestEffort.equals(false)){
+			exceptionRule.expect(ShortReadException.class);
 		}
-		
+
+		int numBytesRead = fi.read(bBuff, this.position, this.bestEffort);
+
+		assertTrue(numBytesRead > 0);
+		assertEquals(new Integer((int)numBytesWritten), new Integer(bBuff.capacity() - bBuff.remaining()));
+
+
+
+		fi.close(true);
+
 	}
 	
 	private String readFromFile(Integer numBytes,String pathFile) {
@@ -221,7 +273,7 @@ public class TestFileInfo {
 		
 		try (FileInputStream fis = new FileInputStream(pathFile)) { //"tmp"+"/"+fileName
         	
-        	int i = 0;
+        	int i;
 
             do {
 
@@ -229,7 +281,8 @@ public class TestFileInfo {
                 i = fis.read(readStr);
                 
                 value = new String(readStr, StandardCharsets.UTF_8);
-               
+				if(value.equals(new String(new byte[numBytes],StandardCharsets.UTF_8)))
+					i = 0;
 
             } while (i != 0);
         	
@@ -245,21 +298,28 @@ public class TestFileInfo {
 	public void testMoveToNewLocation() throws IOException {
 		
 		FileInfo fi = new FileInfo(lf,this.masterKey,this.fileInfoVersion);
-		
+
 		long numBytesWritten = fi.write(this.dataTest2,this.position);
+		b1.position(0);
+
+		assertTrue(numBytesWritten > 0);
 		
 		String result = readFromFile(Math.toIntExact(numBytesWritten),"tmp"+"/"+fileName);
+
+
+		fi.moveToNewLocation(this.dst, this.sizeContent);
+
 		
-		fi.moveToNewLocation(this.dst,this.sizeContent);
-		
-		String resultNewFile = readFromFile(Math.toIntExact(numBytesWritten),"tmp"+"/"+ "temp.log");
-		
-		//check deleted file
-		assertTrue(!Files.exists(Paths.get("tmp"+"/"+fileName)));
-		//check if new file exists
-		assertTrue(Files.exists(Paths.get("tmp"+"/"+this.dst.getName())));
-		
-		assertEquals(result,resultNewFile);
+		String resultNewFile = readFromFile(Math.toIntExact(numBytesWritten),"tmp"+"/"+ this.dst.getName());
+
+		if(!this.dst.getName().equals(lf.getName())) {
+			//check deleted file
+			assertTrue(!Files.exists(Paths.get("tmp" + "/" + fileName)));
+			//check if new file exists
+			assertTrue(Files.exists(Paths.get("tmp" + "/" + this.dst.getName())));
+
+			assertEquals(result, resultNewFile);
+		}
 
 	}
 	
